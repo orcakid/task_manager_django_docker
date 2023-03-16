dfd04e942c810c13fd413b9ed68663de13f140c5
orcakid <orcakid97@gmail.com>
2023-03-16 16:20:56 +0300

    Second commit

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#(rzl0h1o#q2ll37!fw*fisqa&w5n8zu!mx_7ohh#tj4k5s8dn'
+SECRET_KEY = 'django-insecure-+cxy)-r$(rl+q*o16yrzucj-w*&4fx!ypxpt451y1dthd-g5&k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
